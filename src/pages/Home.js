ee1de8b3c41a98e75c2e79c0b64a59a3c8a7f34e
Dino <dino.hopic@gmail.com>
2022-05-06 09:55:23 +0200

    second commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
-// import bannerImg from "../assets/img/banner-vinted.jpg";
+import bannerImg from "../assets/img/banner-vinted.jpg";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   const [data, setData] = useState();
@@ -25,13 +26,44 @@ export default function Home() {
   return isLoading ? (
     <span>En cours de chargement ...</span>
   ) : (
-    data.offers.map((offer) => {
-      return (
-        <div className="offers">
-          <div>{offer.product_name}</div>
-        </div>
-      );
-    })
+    <div>
+      <div className="bannerAdapt">
+        <img src={bannerImg} alt="" />
+      </div>
+
+      <div className="offers container">
+        {data.offers.map((offer, indexOffer) => {
+          return (
+            <div key={indexOffer} className="articles">
+              <div className="nameArticle">
+                {/* <img
+                  key={indexOffer}
+                  src={offer.owner.account.avatar.secure_url}
+                  alt=""
+                ></img> */}
+                <div>{offer.owner.account.username}</div>
+              </div>
+              <Link to={`/offer/${offer._id}`} key={offer._id}>
+                <div>
+                  <img src={offer.product_image.secure_url} alt="" />
+                </div>
+              </Link>
+              <div>{offer.product_price} €</div>
+              <div>
+                {offer.product_details.map((details, indexDetails) => {
+                  return (
+                    <div key={indexDetails}>
+                      <div>{details.TAILLE}</div>
+                      <div>{details.MARQUE}</div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 
   // <div>
